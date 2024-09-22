9c4db7fab9c86eb165035663a4f19cb16c2371a2
erik <erik.slovik1111@gmail.com>
2024-09-22 21:38:50 +0200

    first upload

---
+++ first game/first-game/scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animation_player.play("jump")
+	
+	# Get the input direction: -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+	# Play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+		
+	
+	
+	#Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
